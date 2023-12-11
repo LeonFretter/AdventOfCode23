e2e25a6f00672563dfb0efb35629f6e2ced07807
Leon Fretter <leonfretter@web.de>
2023-12-11 09:04:48 +0100

    Add day 11 part 2

--- 11/main.py
+++ 11/main.py
@@ -1,13 +1,20 @@
 import os
-from universe import createExpandedUniverse, findGalaxies, countSteps
+from universe import createPositionMap, findGalaxies, countSteps
 
 dirname = os.path.dirname(__file__)
 
 with open(os.path.join(dirname, 'input.txt')) as f:
     lines = f.readlines()
 
-    universe = createExpandedUniverse(lines)
-    galaxies = findGalaxies(universe)
+    universe = lines
+    positions = createPositionMap(lines, 2)
+    galaxies = findGalaxies(universe, positions)
     steps = countSteps(galaxies)
 
     print(f"Part 1: {steps}")
+
+    positions = createPositionMap(lines, 1000000)
+    galaxies = findGalaxies(universe, positions)
+    steps = countSteps(galaxies)
+
+    print(f"Part 2: {steps}")
--- 11/universe.py
+++ 11/universe.py
@@ -18,25 +18,37 @@ def isEmptyColumn(lines: list[str], column: int) -> bool:
     return len([x for x in lines if x[column] == '#']) == 0
 
 
-def createExpandedUniverse(lines: list[str]) -> list[str]:
-    expanded: list[str] = []
+def createExpansionMap(lines: list[str], expansion: int) -> list[list[Vec2]]:
+    res: list[list[Vec2]] = []
     for row_idx, line in enumerate(lines):
-        expanded.append(line)
-        if isEmptyRow(lines, row_idx):
-            expanded.append(line)
+        res.append([Vec2(1, 1) for _ in range(len(line))])
 
-    expanded_col_idx = 0
+    for row_idx, line in enumerate(lines):
+        if isEmptyRow(lines, row_idx):
+            for col_idx in range(len(line)):
+                res[row_idx][col_idx].y = expansion
+
     for col_idx in range(len(lines[0])):
         if isEmptyColumn(lines, col_idx):
-            for row_idx, line in enumerate(expanded):
-                expanded[row_idx] = line[:expanded_col_idx] + '.' + line[expanded_col_idx:]
-            expanded_col_idx += 1
-        expanded_col_idx += 1
+            for row_idx in range(len(lines)):
+                res[row_idx][col_idx].x = expansion
 
-    return expanded
+    return res
 
 
-
+def createPositionMap(lines: list[str], expansion: int) -> list[list[Vec2]]:
+    expansions = createExpansionMap(lines, expansion)
+    it = Vec2(0, 0)
+    for row in expansions:
+        it.x = 0
+        next_y = it.y + row[0].y
+        for cell in row:
+            next_x = it.x + cell.x
+            cell.x = it.x
+            cell.y = it.y
+            it.x = next_x
+        it.y = next_y
+    return expansions
 
 
 @dataclass
@@ -47,12 +59,13 @@ class Galaxy:
         return abs(self.position.x - other.position.x) + abs(self.position.y - other.position.y)
 
 
-def findGalaxies(universe: list[str]) -> list[Galaxy]:
+def findGalaxies(universe: list[str], positions: list[list[Vec2]]) -> list[Galaxy]:
     res: list[Galaxy] = []
     for y, row in enumerate(universe):
         for x, cell in enumerate(row):
             if cell == '#':
-                res.append(Galaxy(Vec2(x, y)))
+                pos = positions[y][x]
+                res.append(Galaxy(pos))
     return res
 
 
@@ -86,13 +99,23 @@ if __name__ == "__main__":
 #...#.....
 """
     lines = [x for x in example_txt1.split('\n') if x != '']
-    universe = createExpandedUniverse(lines)
-    for l in universe:
-        print(l)
+    positions = createPositionMap(lines, 2)
 
-    galaxies = findGalaxies(universe)
+    galaxies = findGalaxies(lines, positions)
     assert len(galaxies) == 9
     pairs = buildPairs(galaxies)
     assert len(pairs) == 36
     num_steps = countSteps(galaxies)
     assert num_steps == 374
+
+    positions2 = createPositionMap(lines, 10)
+    galaxies2 = findGalaxies(lines, positions2)
+    assert len(galaxies2) == 9
+    num_steps = countSteps(galaxies2)
+    assert num_steps == 1030
+
+    positions3 = createPositionMap(lines, 100)
+    galaxies3 = findGalaxies(lines, positions3)
+    assert len(galaxies3) == 9
+    num_steps = countSteps(galaxies3)
+    assert num_steps == 8410
