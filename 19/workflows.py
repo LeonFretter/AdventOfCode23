e6df29fd91e59739587e34fff96f15ebe7336200
Leon Fretter <leonfretter@web.de>
2023-12-19 18:04:09 +0100

    Add unfinished day 19 part 2

--- 19/workflows.py
+++ 19/workflows.py
@@ -1,11 +1,12 @@
 from enum import Enum
 from dataclasses import dataclass, field
 from typing import Any, Optional
+from itertools import combinations
 
 
 class Operator(Enum):
-    LESS_THAN = 1
-    GREATER_THAN = 2
+    LESS_THAN = "<"
+    GREATER_THAN = ">"
 
 
 @dataclass
@@ -23,11 +24,27 @@ class Condition:
         else:
             raise RuntimeError("Unknown operator")
 
+    def __str__(self) -> str:
+        return f"{self.var_name}{self.operator.value}{self.value}"
+
+    def invert(self) -> "Condition":
+        if self.operator == Operator.LESS_THAN:
+            return Condition(self.var_name, Operator.GREATER_THAN, self.value - 1)
+        elif self.operator == Operator.GREATER_THAN:
+            return Condition(self.var_name, Operator.LESS_THAN, self.value + 1)
+        else:
+            raise RuntimeError("Unknown operator")
+
 
 @dataclass
 class Rule:
     target: str
     condition: Optional[Condition] = None
+
+    def __str__(self) -> str:
+        if self.condition is None:
+            return self.target
+        return f"{self.condition}:{self.target}"
 
 
 def parseRule(rule_txt: str) -> Rule:
@@ -167,7 +184,7 @@ class WorkflowNode:
             leaves.extend(child.getLeaves())
         return leaves
 
-    def getPaths(self) -> "list[list[WorkflowNode]]":
+    def getPaths(self) -> "list[WorkflowPath]":
         paths: list[list[WorkflowNode]] = []
         leaves = self.getLeaves()
         for leaf in leaves:
@@ -181,6 +198,7 @@ class WorkflowNode:
 
 
 type WorkflowTree = WorkflowNode
+type WorkflowPath = list[WorkflowNode]
 
 
 def buildWorkflowNode(node: WorkflowTree, workflows: list[Workflow]) -> None:
@@ -208,6 +226,163 @@ def buildWorkflowTree(workflows: list[Workflow]) -> WorkflowNode:
     node = WorkflowNode(root)
     buildWorkflowNode(node, workflows)
     return node
+
+
+def filterPaths(paths: list[WorkflowPath]) -> list[WorkflowPath]:
+    res: list[WorkflowPath] = []
+    for p in paths:
+        leave = p[-1]
+        if leave.workflow == "A":
+            res.append(p)
+    return res
+
+
+def getConditions(path: WorkflowPath) -> list[Condition]:
+    """
+        None is interpreted as tautology
+    """
+    res: list[Condition] = []
+    for i in range(len(path) - 1):
+        node = path[i]
+        next_node = path[i + 1]
+        if isinstance(node.workflow, Workflow):
+            next_name = ""
+            if isinstance(next_node.workflow, str):
+                next_name = next_node.workflow
+            else:
+                next_name = next_node.workflow.name
+
+            condition = None
+            prev_conditions = []
+            for r in node.workflow.rules:
+                if r.target == next_name:
+                    condition = r.condition
+                else:
+                    prev_conditions.append(r.condition)
+            prev_inverted = [c.invert() for c in prev_conditions if c is not None]
+            res.extend(prev_inverted)
+            if condition is not None:
+                res.append(condition)
+    return res
+
+
+@dataclass
+class Conjunction:
+    conditions: list[Condition]
+
+    def __str__(self) -> str:
+        return " and ".join(str(c) if c is not None else "*" for c in self.conditions)
+
+    def get_min_val(self, var_name: str) -> int:
+        x = 1
+        for c in self.conditions:
+            if c.var_name == var_name and c.operator == Operator.GREATER_THAN:
+                x = max(x, c.value + 1)
+        return x
+
+    def get_max_val(self, var_name: str) -> int:
+        x = 4000
+        for c in self.conditions:
+            if c.var_name == var_name and c.operator == Operator.LESS_THAN:
+                x = min(x, c.value - 1)
+        return x
+
+
+class Range:
+    def __init__(self, var_name: str = "", conj: Conjunction | None = None) -> None:
+        if var_name != "" and conj is not None:
+            self.min_val = conj.get_min_val(var_name)
+            self.max_val = conj.get_max_val(var_name)
+
+    def count(self) -> int:
+        if self.min_val > self.max_val:
+            return 0
+        return self.max_val - self.min_val + 1
+
+    def copy(self) -> "Range":
+        new_range = Range()
+        new_range.min_val = self.min_val
+        new_range.max_val = self.max_val
+        return new_range
+
+    @staticmethod
+    def intersection(a: "Range", b: "Range") -> "Range":
+        res = Range()
+        res.min_val = max(a.min_val, b.min_val)
+        res.max_val = min(a.max_val, b.max_val)
+        return res
+
+    @staticmethod
+    def union(a: "Range", b: "Range") -> "Range":
+        """
+            Assumes that there's no gap between a and b
+        """
+        res = Range()
+        res.min_val = min(a.min_val, b.min_val)
+        res.max_val = max(a.max_val, b.max_val)
+        return res
+
+    @staticmethod
+    def get_unique(elems: "list[Range]") -> "list[Range]":
+        unions: list[Range] = elems
+        while True:
+            if len(unions) == 1:
+                break
+
+            next_unions = []
+            considered: list[Range] = []
+            for i in range(0, len(unions)):
+                for j in range():
+                    pass
+
+            for i in range(0, len(unions), 2):
+                if i + 1 == len(unions):
+                    next_unions.append(unions[i])
+                    break
+                a = unions[i]
+                b = unions[i + 1]
+                intersection = Range.intersection(a, b)
+                if intersection.count() > 0:
+                    union = Range.union(a, b)
+                    next_unions.append(union)
+                else:
+                    next_unions.append(a)
+                    next_unions.append(b)
+            if sum(u.count() for u in next_unions) == sum(u.count() for u in unions):
+                break
+            unions = next_unions
+        return unions
+
+
+class RangeSet:
+    def __init__(self, conj: Conjunction) -> None:
+        self.x = Range("x", conj)
+        self.m = Range("m", conj)
+        self.a = Range("a", conj)
+        self.s = Range("s", conj)
+
+
+class RangeSetDisjunction:
+    def __init__(self, conjunctions: list[Conjunction]) -> None:
+        self.range_sets = [RangeSet(c) for c in conjunctions]
+
+    def count(self) -> int:
+        xs = [r.x for r in self.range_sets]
+        ms = [r.m for r in self.range_sets]
+        as_ = [r.a for r in self.range_sets]
+        ss = [r.s for r in self.range_sets]
+
+        xs = Range.get_unique(xs)
+        ms = Range.get_unique(ms)
+        as_ = Range.get_unique(as_)
+        ss = Range.get_unique(ss)
+
+        x = sum(r.count() for r in xs)
+        m = sum(r.count() for r in ms)
+        a = sum(r.count() for r in as_)
+        s = sum(r.count() for r in ss)
+
+        return x * m * a * s
 
 
 if __name__ == "__main__":
@@ -240,4 +415,17 @@ hdj{m>838:A,pv}
     res = sum(int(e) for e in accepted)
     assert res == 19114
 
-    buildWorkflowTree(problem_set.workflows)
+    tree = buildWorkflowTree(problem_set.workflows)
+    paths = tree.getPaths()
+    print("path-count: " + str(len(paths)))
+    paths = filterPaths(paths)
+    print("filtered path-count: " + str(len(paths)))
+    conjunctions = [Conjunction(getConditions(p)) for p in paths]
+    print("conjunctions:")
+    for c in conjunctions:
+        print(c)
+
+    range_set_disjunction = RangeSetDisjunction(conjunctions)
+
+    count = range_set_disjunction.count()
+    assert count == 167409079868000
